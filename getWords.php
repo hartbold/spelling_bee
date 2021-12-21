6d257dd55177c51f64da241d3511ea400ab85db3
hartbold <hartbold8@gmail.com>
2021-12-21 20:30:27 +0100

    test file exists

--- getWords.php
+++ getWords.php
@@ -1,6 +1,13 @@
 <?php
 
-$file = file_get_contents("/tmp/palabras_del_dia.json");
+$f = "/tmp/palabras_del_dia.json";
+
+if(file_exists($f)){
+    $file = file_get_contents($f);
+} else {
+    $file = "{error}";
+}
+
 
 header('Content-type:application/json;charset=utf-8');
 echo $file;
