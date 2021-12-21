fb501b902ed4933b11929b3dee1956ce943d2285
hartbold <hartbold8@gmail.com>
2021-12-21 21:10:58 +0100

    conflict

--- getWords.php
+++ getWords.php
@@ -2,14 +2,7 @@
 
 $f = "/tmp/palabras_del_dia.json";
 
-if(file_exists($f)){
-    $file = file_get_contents($f);
-} else {
-    $file = '{
-        "error": '.getcwd().'
-      }';
-}
-
+$file = file_get_contents($f);
 
 header('Content-type:application/json;charset=utf-8');
 echo $file;
